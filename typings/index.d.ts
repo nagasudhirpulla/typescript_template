1248ef5a947181553778675ab84bfb4fea8c0829
nagasudhirpulla <nagasudhirpulla@gmail.com>
2019-08-07 23:14:43 +0530

    first commit

---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/d3/index.d.ts" />
+/// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/mocha/index.d.ts" />
+/// <reference path="modules/react-dom/index.d.ts" />
+/// <reference path="modules/react/index.d.ts" />
